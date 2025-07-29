1cdfb0292e25c5038baddce297e88cec3687ce88
Dunkan <danyakononenko14@gmail.com>
2025-07-29 22:52:20 +0300

    feat: make basic function of Frame with JFrame

--- src/main/java/com/example/services/CalcService.java
+++ src/main/java/com/example/services/CalcService.java
@@ -2,5 +2,17 @@ package com.example.services;
 
 public class CalcService {
 
-  CalcService() {}
+  private final Frame frame;
+
+  private final Panel panel;
+
+  private final Label label;
+
+  CalcService(Frame frame, Panel panel, Label label) {
+    this.frame = frame;
+    this.panel = panel;
+    this.label = label;
+  }
+
+  public void runService() {}
 }
--- src/main/java/com/example/services/Frame.java
+++ src/main/java/com/example/services/Frame.java
@@ -1,5 +1,17 @@
 package com.example.services;
 
+import com.example.enums.Board;
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
-public class Frame extends JFrame {}
+public class Frame extends JFrame {
+
+  Frame() {
+    setSize(Board.WIDTH.getSize(), Board.HIGHT.getSize());
+    setLocationRelativeTo(null);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout());
+    setVisible(true);
+  }
+}
